6f014ebc8508d4ed2e4cceb0398e9bb434caa5c3
Michael Armani <michaelarmani00@gmail.com>
2021-12-06 21:12:08 +0100

    Add Survey Model

---
+++ Models/Survey.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SurveySystem.Models;
+
+public class Survey
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
