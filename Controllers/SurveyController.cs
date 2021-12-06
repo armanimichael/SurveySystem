543d6453238be6cfa04b3a9c938985f7954f3479
Michael Armani <michaelarmani00@gmail.com>
2021-12-06 21:12:31 +0100

    Add Survey Controller

---
+++ Controllers/SurveyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveySystem.Data;
+using SurveySystem.Models;
+
+namespace SurveySystem.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SurveyController : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public SurveyController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet(Name = "/")]
+    public IEnumerable<Survey> Get()
+    {
+        return _dbContext.Surveys.ToList();
+    }
+}
