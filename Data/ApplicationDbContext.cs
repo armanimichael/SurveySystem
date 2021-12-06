5059a032c82f65000f7b6cda7d8702d002bc27bb
Michael Armani <michaelarmani00@gmail.com>
2021-12-06 21:12:23 +0100

    Add DB Context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveySystem.Models;
+
+namespace SurveySystem.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Survey> Surveys { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+}
