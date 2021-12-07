df6875ed541e1a714e473e93192ca65e5882dc70
Michael Armani <michaelarmani00@gmail.com>
2021-12-07 20:15:41 +0100

    Create Jwt Service

---
+++ services/JWTService/IJwtService.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Claims;
+
+namespace SurveySystem.services.JWTService;
+
+public interface IJwtService
+{
+    public (string token, DateTime expiration) GenerateToken(IEnumerable<Claim> authClaims);
+}
