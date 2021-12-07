e5bc5abe15b082ee21a283710a30eff5c0e4a047
Michael Armani <michaelarmani00@gmail.com>
2021-12-07 19:47:32 +0100

    Better User Registration Model data annotation

--- Models/UserRegistrationModel.cs
+++ Models/UserRegistrationModel.cs
@@ -4,10 +4,12 @@ namespace SurveySystem.Models;
 
 public class UserRegistrationModel
 {
+    [Key]
     [Required(ErrorMessage = "Username is required")]
-    [MaxLength(50,ErrorMessage = "Username cannot be more than {0} chars long")]
+    [MaxLength(50,ErrorMessage = "Username cannot be more than 50 characters long")]
     public string Username { get; set; }
     
+    [Key]
     [EmailAddress(ErrorMessage = "Invalid email address")]
     [Required(ErrorMessage = "Email is required")]
     public string Email { get; set; }
@@ -16,5 +18,6 @@ public class UserRegistrationModel
     public string Password { get; set; }
     
     [Required(ErrorMessage = "Password confirmation is required")]
+    [Compare("Password", ErrorMessage = "Password and Confirmed Password must match")]
     public string PasswordConfirm { get; set; }
 }
