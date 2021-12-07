fb0fcf6b70e92025c2951a9fb96982ab877b49ce
Michael Armani <michaelarmani00@gmail.com>
2021-12-07 20:38:38 +0100

    Create generic error response

---
+++ Models/ApiResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace SurveySystem.Models;
+
+public class ApiResponse
+{
+    public string? Message { get; set; } = null;
+    public bool Success { get; set; }
+    public IEnumerable<string>? Errors { get; set; }
+}
