a39bdb3eedef3cc075b51ee014ed4a994b0b3258
Michael Armani <michaelarmani00@gmail.com>
2021-12-07 21:58:44 +0100

    Create UserService's Register method

---
+++ services/UserService/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveySystem.Models;
+
+namespace SurveySystem.services.UserService;
+
+public interface IUserService
+{
+    public Task<ApiResponse> Register(UserRegistrationModel registrationModel);
+    public Task<ApiResponse> Login(UserLoginModel loginModel);
+}
---
+++ services/UserService/UserService.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Identity;
+using SurveySystem.Models;
+
+namespace SurveySystem.services.UserService;
+
+public class UserService : IUserService
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public UserService(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    private async Task<bool> IsUserAlreadyRegistered(string username, string email)
+    {
+        bool usernameExists = await _userManager.FindByNameAsync(username) != null;
+        bool emailExists = await _userManager.FindByEmailAsync(email) != null;
+        return usernameExists || emailExists;
+    }
+
+    private static ApiResponse RegistrationErrorResponse(IdentityResult result)
+    {
+        return new ApiResponse()
+        {
+            Message = "Registration error.",
+            Success = false,
+            Errors = result.Errors.Select(err => $"{err.Code}: {err.Description}"),
+            HttpStatusCode = (int)HttpStatusCode.InternalServerError
+        };
+    }
+
+    private async Task<ApiResponse> CreateUser(string email, string username, string password)
+    {
+        var newUser = new IdentityUser() { Email = email, UserName = username };
+        
+        IdentityResult? result = await _userManager.CreateAsync(newUser, password);
+        return !result.Succeeded
+            ? RegistrationErrorResponse(result)
+            : DefaultResponses.UserRegisteredResponse;
+    }
+
+    public async Task<ApiResponse> Register(UserRegistrationModel registrationModel)
+    {
+        string username = registrationModel.Username;
+        string email = registrationModel.Email;
+        string password = registrationModel.Password;
+        if (await IsUserAlreadyRegistered(username, email))
+        {
+            return DefaultResponses.UserExistsResponse;
+        }
+
+        return await CreateUser(email, username, password);
+    }
+
+    public Task<ApiResponse> Login(UserLoginModel loginModel)
+    {
+        throw new NotImplementedException();
+    }
+}
