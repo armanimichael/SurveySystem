d44f97bbe08f04fb756f2259c05af9d69a404afc
Michael Armani <michaelarmani00@gmail.com>
2021-12-07 22:26:29 +0100

    Add metadata to ApiReponse model

--- Models/ApiResponse.cs
+++ Models/ApiResponse.cs
@@ -7,6 +7,7 @@ public class ApiResponse
     public string? Message { get; set; }
     public bool Success { get; set; }
     public IEnumerable<string>? Errors { get; set; }
+    public object? MetaData { get; set; }
 
     [JsonIgnore]
     public int HttpStatusCode { get; set; }
