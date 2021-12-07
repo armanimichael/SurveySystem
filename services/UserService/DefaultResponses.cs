5829a63e2f7878503a63c57c4f051b555c632a43
Michael Armani <michaelarmani00@gmail.com>
2021-12-07 21:57:27 +0100

    Create UserService ready to use responses

---
+++ services/UserService/DefaultResponses.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using SurveySystem.Models;
+
+namespace SurveySystem.services.UserService;
+
+public static class DefaultResponses
+{
+    public static readonly ApiResponse UserExistsResponse = new()
+    {
+        Message = "User already exists.",
+        Success = false,
+        HttpStatusCode = (int)HttpStatusCode.Conflict
+    };
+
+    public static readonly ApiResponse UserRegisteredResponse = new()
+    {
+        Message = "User registered successfully.",
+        Success = true,
+        HttpStatusCode = (int)HttpStatusCode.OK
+    };
+}
