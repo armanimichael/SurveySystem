4136dda9018cdc30cbef2913977e2d4a1732eef4
Michael Armani <michaelarmani00@gmail.com>
2021-12-07 22:27:51 +0100

    Add login to UserService (with actual password checking)

--- services/UserService/UserService.cs
+++ services/UserService/UserService.cs
@@ -1,22 +1,28 @@
 ﻿using System.Net;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.JsonWebTokens;
 using SurveySystem.Models;
+using SurveySystem.services.JWTService;
 
 namespace SurveySystem.services.UserService;
 
 public class UserService : IUserService
 {
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly IJwtService _jwtService;
 
-    public UserService(UserManager<IdentityUser> userManager)
+    public UserService(UserManager<IdentityUser> userManager, IJwtService jwtService)
     {
         _userManager = userManager;
+        _jwtService = jwtService;
     }
 
-    private async Task<bool> IsUserAlreadyRegistered(string username, string email)
+    private async Task<bool> IsUserAlreadyRegistered(string? username, string? email)
     {
-        bool usernameExists = await _userManager.FindByNameAsync(username) != null;
-        bool emailExists = await _userManager.FindByEmailAsync(email) != null;
+        bool usernameExists = !string.IsNullOrEmpty(username) && await _userManager.FindByNameAsync(username) != null;
+        bool emailExists = !string.IsNullOrEmpty(email) && await _userManager.FindByEmailAsync(email) != null;
+
         return usernameExists || emailExists;
     }
 
@@ -31,20 +37,45 @@ public class UserService : IUserService
         };
     }
 
-    private async Task<ApiResponse> CreateUser(string email, string username, string password)
+    private async Task<ApiResponse> CreateUser(string? email, string username, string password)
     {
         var newUser = new IdentityUser() { Email = email, UserName = username };
-        
+
         IdentityResult? result = await _userManager.CreateAsync(newUser, password);
         return !result.Succeeded
             ? RegistrationErrorResponse(result)
             : DefaultResponses.UserRegisteredResponse;
     }
 
+    private ApiResponse GetJwtToken(IEnumerable<Claim> authClaims)
+    {
+        (string token, DateTime expiration) = _jwtService.GenerateToken(authClaims);
+
+        ApiResponse successResponse = DefaultResponses.UserLoggedInResponse;
+        successResponse.MetaData = new
+        {
+            token,
+            expiration
+        };
+        return successResponse;
+    }
+
+    private async Task<List<Claim>> CreateUserClaims(IdentityUser user)
+    {
+        IList<string>? userRoles = await _userManager.GetRolesAsync(user);
+        var authClaims = new List<Claim>
+        {
+            new(ClaimTypes.Name, user.UserName),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+        authClaims.AddRange(userRoles.Select(userRole => new Claim(ClaimTypes.Role, userRole)));
+        return authClaims;
+    }
+
     public async Task<ApiResponse> Register(UserRegistrationModel registrationModel)
     {
         string username = registrationModel.Username;
-        string email = registrationModel.Email;
+        string? email = registrationModel.Email;
         string password = registrationModel.Password;
         if (await IsUserAlreadyRegistered(username, email))
         {
@@ -54,8 +85,22 @@ public class UserService : IUserService
         return await CreateUser(email, username, password);
     }
 
-    public Task<ApiResponse> Login(UserLoginModel loginModel)
+    public async Task<ApiResponse> Login(UserLoginModel loginModel)
     {
-        throw new NotImplementedException();
+        bool userExists = await IsUserAlreadyRegistered(loginModel.Username, null);
+        if (!userExists)
+        {
+            return DefaultResponses.IncorrectLoginData;
+        }
+
+        IdentityUser user = await _userManager.FindByNameAsync(loginModel.Username);
+        bool validPassword = await _userManager.CheckPasswordAsync(user, loginModel.Password);
+        if (!validPassword)
+        {
+            return DefaultResponses.IncorrectLoginData;
+        }
+        
+        List<Claim> authClaims = await CreateUserClaims(user);
+        return GetJwtToken(authClaims);
     }
 }
