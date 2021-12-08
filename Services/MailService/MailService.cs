320eafb50540ac72721850b165e41c6a274a4710
Michael Armani <michaelarmani00@gmail.com>
2021-12-08 19:47:49 +0100

    Refractoring MailService

--- Services/MailService/MailService.cs
+++ Services/MailService/MailService.cs
@@ -12,14 +12,15 @@ public class MailService : IMailService
     private readonly MailSenderConfiguration _senderConfiguration;
     private readonly string _from;
     private readonly string _fromName;
-    
-    public MailService(IConfiguration configuration, LinkGenerator linkGenerator, IHttpContextAccessor httpContextAccessor)
+
+    public MailService(IConfiguration configuration, LinkGenerator linkGenerator,
+        IHttpContextAccessor httpContextAccessor)
     {
         _linkGenerator = linkGenerator;
         _httpContextAccessor = httpContextAccessor;
         _from = configuration["Email:From"];
         _fromName = configuration["Email:FromName"];
-        
+
         _senderConfiguration = new MailSenderConfiguration()
         {
             Host = configuration["Smtp:Host"],
@@ -36,16 +37,16 @@ public class MailService : IMailService
             .Connect(_senderConfiguration)
             .From(_from, _fromName);
     }
-    
+
     private string GetVerificationUrl(string userId, string token)
     {
         token = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
         userId = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(userId));
-        string url =
-            _linkGenerator.GetUriByAction(_httpContextAccessor.HttpContext!, "Verify", "Account", new { token, userId })!;
-        return url;
+
+        HttpContext context = _httpContextAccessor.HttpContext!;
+        return _linkGenerator.GetUriByAction(context, "Verify", "Account", new { token, userId })!;
     }
-    
+
     private string CreateEmailVerificationBody(string userId, string token)
     {
         string url = GetVerificationUrl(userId, token);
@@ -60,7 +61,7 @@ public class MailService : IMailService
     public void SendConfirmationToken(string userEmail, string userId, string token)
     {
         string body = CreateEmailVerificationBody(token, userId);
-        
+
         DefaultMailSender()
             .SendTo(userEmail, userEmail)
             .Done()
