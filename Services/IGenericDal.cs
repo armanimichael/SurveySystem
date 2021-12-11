de4499cfb13e23e937b6a1eefdb50ac7c75378a3
Michael Armani <michaelarmani00@gmail.com>
2021-12-11 20:11:08 +0100

    Add SurveyService create method

--- Services/IGenericDal.cs
+++ Services/IGenericDal.cs
@@ -4,4 +4,5 @@ public interface IGenericDal<T>
 {
     public Task<IList<T>> Get();
     public Task<T?> Get(Guid id);
+    public Task<T?> Create(T item);
 }
--- Services/SurveyService/SurveyService.cs
+++ Services/SurveyService/SurveyService.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SurveySystem.Data;
 using SurveySystem.Models;
+using SurveySystem.services.UserService;
 
 namespace SurveySystem.services.SurveyService;
 
 public class SurveyService : ISurveyService
 {
     private readonly ApplicationDbContext _dbContext;
+    private readonly IUserService _userService;
 
-    public SurveyService(ApplicationDbContext dbContext)
+    public SurveyService(ApplicationDbContext dbContext, IUserService userService)
     {
         _dbContext = dbContext;
+        _userService = userService;
     }
 
     public async Task<IList<Survey>> Get()
@@ -21,5 +24,23 @@ public class SurveyService : ISurveyService
     public async Task<Survey?> Get(Guid id)
     {
         return await _dbContext.Surveys.SingleOrDefaultAsync(s => s.Id.Equals(id));
+    }
+
+    public async Task<Survey?> Create(Survey survey)
+    {
+        if (!await IsUnique(survey))
+        {
+            return null;
+        }
+
+        survey.UserId = (await _userService.GetCurrentUser()).Id;
+        await _dbContext.AddAsync(survey);
+        await _dbContext.SaveChangesAsync();
+        return survey;
+    }
+
+    private async Task<bool> IsUnique(Survey survey)
+    {
+        return !await _dbContext.Surveys.AnyAsync(s => s.Name == survey.Name);
     }
 }
