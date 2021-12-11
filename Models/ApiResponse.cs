0246a94cf77035fa8eec2baa2703025a62c779d4
Michael Armani <michaelarmani00@gmail.com>
2021-12-11 19:33:50 +0100

    Add ApiResponse constructors

--- Models/ApiResponse.cs
+++ Models/ApiResponse.cs
@@ -4,6 +4,18 @@ namespace SurveySystem.Models;
 
 public class ApiResponse
 {
+    public ApiResponse()
+    {
+        
+    }
+    
+    public ApiResponse(bool success, object metaData, int httpStatusCode)
+    {
+        Success = success;
+        MetaData = metaData;
+        HttpStatusCode = httpStatusCode;
+    }
+    
     public string? Message { get; set; }
     public bool Success { get; set; }
     public IEnumerable<string>? Errors { get; set; }
