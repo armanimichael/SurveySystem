3eda9d1acf4addb738127d761f3f968871ec1af5
Michael Armani <michaelarmani00@gmail.com>
2021-12-11 19:45:08 +0100

    Add default reponses for SurveyService

---
+++ Services/SurveyService/DefaultReponses.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using SurveySystem.Models;
+
+namespace SurveySystem.services.SurveyService;
+
+public class DefaultReponses
+{
+    public static readonly ApiResponse Error = new()
+    {
+        Message = "There was an error retriving the selected survey.",
+        Success = false,
+        HttpStatusCode = (int)HttpStatusCode.InternalServerError,
+    };
+
+    public static readonly ApiResponse NotFound = new()
+    {
+        Message = "Could not find any survey.",
+        Success = false,
+        HttpStatusCode = (int)HttpStatusCode.NotFound
+    };
+}
