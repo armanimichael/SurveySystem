1e21ae981ccbb13b969aadd7f697b376cd307df4
Michael Armani <michaelarmani00@gmail.com>
2021-12-26 14:07:23 +0100

    Add question model

---
+++ Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SurveySystem.Models;
+
+public class Question
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    public bool IsMultipleChoices { get; set; }
+}
