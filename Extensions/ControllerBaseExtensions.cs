b06df36ab898391ce22c21b62833f2076bdeb908
Michael Armani <michaelarmani00@gmail.com>
2021-12-26 13:56:36 +0100

    Add CustomApiResponse ControllerBase extension method

---
+++ Extensions/ControllerBaseExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveySystem.Models;
+
+namespace SurveySystem.Extensions;
+
+public static class ControllerBaseExtensions
+{
+    public static IActionResult CustomApiResponse(this ControllerBase controllerBase, ApiResponse reponse) =>
+        controllerBase.StatusCode(reponse.HttpStatusCode, reponse);
+}
