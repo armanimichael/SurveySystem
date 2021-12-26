7180c4a1cfe32104a5c0829a9e337da6aeaeedca
Michael Armani <michaelarmani00@gmail.com>
2021-12-26 15:22:26 +0100

    Add ApiResponses for Question

---
+++ ApiResponses/QuestionApiResponses.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using SurveySystem.Models;
+
+namespace SurveySystem.ApiResponses;
+
+public class QuestionApiResponses
+{
+    public static readonly ApiResponse NotFound = new()
+    {
+        Message = "Could not find any question.",
+        Success = false,
+        HttpStatusCode = (int)HttpStatusCode.NotFound
+    };
+    
+    public static readonly ApiResponse GetError = new()
+    {
+        Message = "There was an error retriving the selected question.",
+        Success = false,
+        HttpStatusCode = (int)HttpStatusCode.InternalServerError,
+    };
+}
