5618cd7eee6888b5584280ff1b07fbaa6bbcd54a
Michael Armani <michaelarmani00@gmail.com>
2021-12-26 13:49:27 +0100

    Add user verification error API reponse

--- ApiResponses/AccountApiResponses.cs
+++ ApiResponses/AccountApiResponses.cs
@@ -19,42 +19,42 @@ public static class AccountApiResponses
         Success = true,
         HttpStatusCode = (int)HttpStatusCode.OK
     };
-    
+
     public static readonly ApiResponse UserLoggedInResponse = new()
     {
         Message = "User logged in successfully.",
         Success = true,
         HttpStatusCode = (int)HttpStatusCode.OK
     };
-    
+
     public static readonly ApiResponse IncorrectLoginData = new()
     {
         Message = "Incorrect user or password.",
         Success = false,
         HttpStatusCode = (int)HttpStatusCode.Unauthorized
     };
-    
+
     public static readonly ApiResponse UnverifiedEmail = new()
     {
         Message = "You must verify your account, check your email.",
         Success = false,
         HttpStatusCode = (int)HttpStatusCode.Unauthorized
     };
-    
+
     public static readonly ApiResponse ConfirmationEmailError = new()
     {
         Message = "There was an error while sending the confirmation email.",
         Success = false,
         HttpStatusCode = (int)HttpStatusCode.InternalServerError
     };
-    
+
     public static readonly ApiResponse ConfirmationError = new()
     {
         Message = "Could not confirm user.",
         Success = false,
         HttpStatusCode = (int)HttpStatusCode.Unauthorized
     };
-    
+
     public static readonly ApiResponse ConfirmationSuccess = new()
     {
         Message = "User email confirmed successfully.",
@@ -68,14 +68,21 @@ public static class AccountApiResponses
         Success = false,
         HttpStatusCode = (int)HttpStatusCode.InternalServerError
     };
-    
+
     public static readonly ApiResponse RegistrationError = new()
     {
         Message = "There was an error registering the user.",
         Success = false,
         HttpStatusCode = (int)HttpStatusCode.InternalServerError
     };
-    
+
+    public static readonly ApiResponse RegistrationTokenVerificationError = new()
+    {
+        Message = "There was an error verifing the user.",
+        Success = false,
+        HttpStatusCode = (int)HttpStatusCode.InternalServerError
+    };
+
     public static ApiResponse GenerateRegistrationErrorResponse(IdentityResult result)
     {
         return new ApiResponse()
