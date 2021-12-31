eb99963435da190ea653f2f2f04bd340f657a5d7
Michael Armani <michaelarmani00@gmail.com>
2021-12-31 12:48:25 +0100

    Create QuestionDto.cs

---
+++ Dtos/QuestionDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SurveySystem.Dtos;
+
+[Serializable]
+public class QuestionDto
+{
+    [Required]
+    [MaxLength(100)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    public string Description { get; set; } = null!;
+    
+    [Required]
+    public Guid SurveyId { get; set; }
+}
