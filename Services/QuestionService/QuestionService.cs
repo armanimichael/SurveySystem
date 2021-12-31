aef937823a5d0038fb865b5b2fa213995983f206
Michael Armani <michaelarmani00@gmail.com>
2021-12-31 13:01:06 +0100

    Allow Questions creation

--- Services/QuestionService/QuestionService.cs
+++ Services/QuestionService/QuestionService.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SurveySystem.ApiResponses;
 using SurveySystem.Data;
+using SurveySystem.Dtos;
 using SurveySystem.Models;
+using SurveySystem.services.SurveyService;
 using SurveySystem.services.UserService;
 
 namespace SurveySystem.services.QuestionService;
@@ -9,11 +12,13 @@ public class QuestionService : IQuestionService
 {
     private readonly ApplicationDbContext _dbContext;
     private readonly IUserService _userService;
+    private readonly ISurveyService _surveyService;
 
-    public QuestionService(ApplicationDbContext dbContext, IUserService userService)
+    public QuestionService(ApplicationDbContext dbContext, IUserService userService, ISurveyService surveyService)
     {
         _dbContext = dbContext;
         _userService = userService;
+        _surveyService = surveyService;
     }
 
     public async Task<Question?> Get(Guid id)
@@ -25,13 +30,28 @@ public class QuestionService : IQuestionService
             .SingleOrDefaultAsync(q => q.Id.Equals(id));
     }
 
-    public Task<Question?> Create(Question question)
+    public async Task<Question?> Create(Question question)
     {
-        throw new NotImplementedException();
+        if (!await IsUnique(question))
+            return null;
+
+        await _dbContext.Questions.AddAsync(question);
+        await _dbContext.SaveChangesAsync();
+        return question;
     }
 
     public Task<ApiResponse> Update(Question question)
     {
         throw new NotImplementedException();
+    }
+
+    private async Task<bool> IsUnique(Question question)
+    {
+        var surveyId = question.SurveyId;
+        var title = question.Title; 
+        
+        return await _dbContext
+            .Questions
+            .SingleOrDefaultAsync(q => q.SurveyId == surveyId && q.Title == title) == null;
     }
 }
