5911922dbe5c320b0ad74a9fa82dbabbd44eb782
Michael Armani <michaelarmani00@gmail.com>
2022-01-04 17:19:44 +0100

    Forgive nulls

--- Models/RefreshToken.cs
+++ Models/RefreshToken.cs
@@ -7,15 +7,15 @@ namespace SurveySystem.Models;
 public class RefreshToken
 {
     [Key]
-    public string Token { get; set; }
-    
-    public string JwtId { get; set; }
+    public string Token { get; set; } = null!;
+
+    public string JwtId { get; set; } = null!;
     public bool IsUsed { get; set; }
     public bool IsRevoked { get; set; }
     public DateTime AddedDate { get; set; }
     public DateTime ExpiryDate { get; set; }
 
     [ForeignKey(nameof(UserId))]
-    public IdentityUser User {get;set;}
-    public string UserId { get; set; }
+    public IdentityUser User {get;set;} = null!;
+    public string UserId { get; set; } = null!;
 }
