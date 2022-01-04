164d72eaa15884f98c99519a95125c4f5e6134fd
Michael Armani <michaelarmani00@gmail.com>
2022-01-04 17:13:23 +0100

    Create AuthResult.cs

---
+++ Models/AuthResult.cs
@@ -0,0 +1,13 @@
+﻿namespace SurveySystem.Models;
+
+public class AuthResult
+{
+    public string Token {get; set;}
+    public string RefreshToken {get; set;}
+
+    public AuthResult(string token, string refreshToken)
+    {
+        Token = token;
+        RefreshToken = refreshToken;
+    }
+}
